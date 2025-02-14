195ebd0111fba025273836c33b5309eedf37f421
venuchilukala <venuchilukala07@gmail.com>
2025-02-14 17:33:49 +0530

    sockets and io

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const http = require('http')
 const path = require('path')
 const express = require('express')
+const {Server} = require('socket.io')
 
 const app = express()
 const PORT = 9000 
 const server = http.createServer(app)
+const io = new Server(server)
+
+// Socket connection
+io.on('connection', (socket) => {
+    socket.on('user-message',(message)=>{
+        io.emit('message', message)
+    })
+})
 
 // Middlewares
 app.use(express.static(path.resolve('./public')))
 
-// Routes
+// Routes 
 app.get('/', (req, res) => {
     return res.sendFile('/public/index.html')
 })
